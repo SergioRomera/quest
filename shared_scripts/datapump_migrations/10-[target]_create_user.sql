1ef67796ade5c21cec9c609afc391431f3140787
SergioRomera <sergio.romera@gmail.com>
2019-07-15 14:18:38 +0200

    Update 10-[target]_create_user.sql

--- shared_scripts/datapump_migrations/10-[target]_create_user.sql
+++ shared_scripts/datapump_migrations/10-[target]_create_user.sql
@@ -2,9 +2,9 @@
 --drop directory data_pump_dir_sample;
 --create directory data_pump_dir_sample as '&2';
 
-WHENEVER SQLERROR EXIT SQL.SQLCODE
 
 drop user &1 cascade;
+WHENEVER SQLERROR EXIT SQL.SQLCODE
 create user &1 identified by &1;
 --grant create session, resource to &1;
 --alter user &1 quota 1000M on users;
