d182502c1d2d6336a056c95e9a65cba5b1c8f45a
SergioRomera <sergio.romera@gmail.com>
2019-07-31 11:25:03 +0200

    Update foglight_auto_start.sh

--- node4_foglight/scripts/foglight_auto_start.sh
+++ node4_foglight/scripts/foglight_auto_start.sh
@@ -1,23 +1,32 @@
 #! /bin/sh
 
 cd /etc/init.d
-\cp foglight foglight.old
-\cp foglight-FMS foglight-FMS.old
+# if [ -f "foglight" ]
+# then
+  # \cp foglight foglight.old
+# fi
+# if [ -f "foglight-FMS" ]
+# then
+  # \cp foglight-FMS foglight-FMS.old
+# fi
+
 \cp /home/foglight/Quest/Foglight/scripts/init.d/RedHat/* /etc/init.d/.
 
 sed -i -e 's/@@FOGLIGHT_HOME_PATH@@/\/home\/foglight\/Quest\/Foglight/g' /etc/init.d/foglight
 sed -i -e 's/@@FOGLIGHT_HOME_PATH@@/\/home\/foglight\/Quest\/Foglight/g' /etc/init.d/foglight-FMS
 
 
-sed -i -e 's/$FOGLIGHT_START/su - foglight -c "$FOGLIGHT_START"/g' /etc/init.d/foglight
+
+#sed -i -e 's/$FOGLIGHT_START/su - foglight -c "$FOGLIGHT_START"/g' /etc/init.d/foglight
+sed -i -e 's/$FOGLIGHT_START/su - foglight -c "rm -f \/home\/foglight\/Quest\/Foglight\/state\/.Fog* \&\& $FOGLIGHT_START"/g' /etc/init.d/foglight
 sed -i -e 's/$FOGLIGHT_STOP/su - foglight -c "$FOGLIGHT_STOP"/g' /etc/init.d/foglight
 sed -i -e 's/$0 stop/su - foglight -c "$0 stop"/g' /etc/init.d/foglight
-sed -i -e 's/$0 start/su - foglight -c "$0 start"/g' /etc/init.d/foglight
+sed -i -e 's/$0 start/su - foglight -c "rm -f \/home\/foglight\/Quest\/Foglight\/state\/.Fog* \&\& $0 start"/g' /etc/init.d/foglight
 
-sed -i -e 's/$FOGLIGHT_START/su - foglight -c "$FOGLIGHT_START"/g' /etc/init.d/foglight-FMS
+sed -i -e 's/$FOGLIGHT_START/su - foglight -c "rm -f \/home\/foglight\/Quest\/Foglight\/state\/.Fog* \&\& $FOGLIGHT_START"/g' /etc/init.d/foglight-FMS
 sed -i -e 's/$FOGLIGHT_STOP/su - foglight -c "$FOGLIGHT_STOP"/g' /etc/init.d/foglight-FMS
 sed -i -e 's/$0 stop/su - foglight -c "$0 stop"/g' /etc/init.d/foglight-FMS
-sed -i -e 's/$0 start/su - foglight -c "$0 start"/g' /etc/init.d/foglight-FMS
+sed -i -e 's/$0 start/su - foglight -c "rm -f \/home\/foglight\/Quest\/Foglight\/state\/.Fog* \&\& $0 start"/g' /etc/init.d/foglight-FMS
 
 chkconfig --add foglight-FMS
 #chkconfig --del foglight-FMS
